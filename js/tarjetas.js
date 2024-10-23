9045af2e9143bef084ad1a6880eedbc16d6e5592
Paloma González-Ripoll <palgoncan05@iesfidiana.es>
2024-10-23 11:05:40 +0200

    cambio

--- js/tarjetas.js
+++ js/tarjetas.js
@@ -1,8 +1,9 @@
 const flipCards = document.querySelectorAll(".flip-card");
 
-flipCards.forEach((card) => {
+flipCards.forEach((card, index) => {
   card.addEventListener("click", () => {
     const innerCard = card.querySelector(".flip-card-inner");
+
     innerCard.classList.toggle("rotate");
   });
 });
